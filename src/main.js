f9c4d8dec0e329ca96ae097d980a782fc8af7e7c
Tesolite <pavel.kotka@gmail.com>
2024-08-08 22:39:16 +0100

    timer made functional

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-function randomColour() {
+var randomColour = function () {
     var random = Math.floor(Math.random() * 8);
     var colour = [
         "red",
@@ -11,8 +11,8 @@ function randomColour() {
         "purple",
     ];
     return colour[random];
-}
-function randomWord() {
+};
+var randomWord = function () {
     var random = Math.floor(Math.random() * 8);
     var word = [
         "red",
@@ -25,10 +25,60 @@ function randomWord() {
         "purple",
     ];
     return word[random];
-}
-function gameRound() {
-    var colour = randomColour();
-    var word = randomWord();
-    var round = [colour, word];
-    return round;
-}
+};
+//function for generating game prompt.
+var gameRound = function (colour, word) {
+    var prompt = document.getElementById("game-prompt");
+    if (prompt) {
+        // Remove all previous classes if they exist
+        prompt.classList.remove("text-red-500", "text-orange-500", "text-yellow-500", "text-green-500", "text-blue-500", "text-pink-500", "text-cyan-500", "text-purple-500");
+        // Set the prompt to the generated colour
+        prompt.textContent = word;
+        //Set the font colour of word to the generated colour
+        switch (colour) {
+            case "red":
+                prompt.classList.add("text-red-500");
+                break;
+            case "orange":
+                prompt.classList.add("text-orange-500");
+                break;
+            case "yellow":
+                prompt.classList.add("text-yellow-500");
+                break;
+            case "green":
+                prompt.classList.add("text-green-500");
+                break;
+            case "blue":
+                prompt.classList.add("text-blue-500");
+                break;
+            case "pink":
+                prompt.classList.add("text-pink-500");
+                break;
+            case "cyan":
+                prompt.classList.add("text-cyan-500");
+                break;
+            case "purple":
+                prompt.classList.add("text-purple-500");
+                break;
+        }
+    }
+};
+//Timer function
+var displayTimer = function () {
+    var timerDisplay = document.getElementById("game-timer");
+    var timer = Date.now() + 1000 * 60;
+    var updateInterval = setInterval(function () {
+        var currentTime = Date.now();
+        var timeLeft = timer - currentTime;
+        if (timeLeft > 0) {
+            timerDisplay.textContent = parseFloat(String(timeLeft / 1000)).toFixed(2);
+        }
+        else {
+            timerDisplay.textContent = "meow";
+            console.log("Time is 0");
+            clearInterval(updateInterval);
+        }
+    }, 25);
+};
+displayTimer();
+gameRound(randomColour(), randomWord());
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-function randomColour() {
+const randomColour = (): string => {
   let random: number = Math.floor(Math.random() * 8);
   const colour: string[] = [
     "red",
@@ -11,9 +11,9 @@ function randomColour() {
     "purple",
   ];
   return colour[random];
-}
+};
 
-function randomWord() {
+const randomWord = (): string => {
   let random: number = Math.floor(Math.random() * 8);
   const word: string[] = [
     "red",
@@ -26,13 +26,76 @@ function randomWord() {
     "purple",
   ];
   return word[random];
-}
+};
 
-function gameRound() {
-  let colour: string = randomColour();
-  let word: string = randomWord();
+//function for generating game prompt.
+const gameRound = (colour: string, word: string) => {
+  const prompt = document.getElementById("game-prompt");
 
-  let round: string[] = [colour, word];
+  if (prompt) {
+    // Remove all previous classes if they exist
+    prompt.classList.remove(
+      "text-red-500",
+      "text-orange-500",
+      "text-yellow-500",
+      "text-green-500",
+      "text-blue-500",
+      "text-pink-500",
+      "text-cyan-500",
+      "text-purple-500",
+    );
 
-  return round;
-}
+    // Set the prompt to the generated colour
+    prompt.textContent = word;
+
+    //Set the font colour of word to the generated colour
+    switch (colour) {
+      case "red":
+        prompt.classList.add("text-red-500");
+        break;
+      case "orange":
+        prompt.classList.add("text-orange-500");
+        break;
+      case "yellow":
+        prompt.classList.add("text-yellow-500");
+        break;
+      case "green":
+        prompt.classList.add("text-green-500");
+        break;
+      case "blue":
+        prompt.classList.add("text-blue-500");
+        break;
+      case "pink":
+        prompt.classList.add("text-pink-500");
+        break;
+      case "cyan":
+        prompt.classList.add("text-cyan-500");
+        break;
+      case "purple":
+        prompt.classList.add("text-purple-500");
+        break;
+    }
+  }
+};
+
+//Timer function
+
+const displayTimer = () => {
+  const timerDisplay = document.getElementById("game-timer")!;
+  let timer: number = Date.now() + 1000 * 60;
+
+  const updateInterval = setInterval(() => {
+    const currentTime: number = Date.now();
+    const timeLeft: number = timer - currentTime;
+    if (timeLeft > 0) {
+      timerDisplay.textContent = parseFloat(String(timeLeft / 1000)).toFixed(2);
+    } else {
+      timerDisplay.textContent = "meow";
+      console.log("Time is 0");
+      clearInterval(updateInterval);
+    }
+  }, 25);
+};
+
+displayTimer();
+gameRound(randomColour(), randomWord());
